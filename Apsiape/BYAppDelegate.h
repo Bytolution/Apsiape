de0342c8687e0e45f239c677d38b341ae4e1ad97
Bytolution <github@bytolution.com>
2013-02-27 19:38:05 +0100

    Initial Commit

---
+++ Apsiape/BYAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BYAppDelegate.h
+//  Apsiape
+//
+//  Created by Dario Lass on 27.02.13.
+//  Copyright (c) 2013 Bytolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BYViewController;
+
+@interface BYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) BYViewController *viewController;
+
+@end
