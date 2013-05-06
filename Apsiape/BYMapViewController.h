32cb81e4724221f2ece7a39a770f7f03ca956600
Dario Lass <github@bytolution.com>
2013-05-06 17:05:12 +0200

    added BYMapViewController and removed the old animation overlay

---
+++ Apsiape/BYMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  BYMapViewController.h
+//  Apsiape
+//
+//  Created by Dario Lass on 06.05.13.
+//  Copyright (c) 2013 Bytolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BYMapViewController : UIViewController
+
+@end
---
+++ Apsiape/BYMapViewController.m
@@ -0,0 +1,38 @@
+//
+//  BYMapViewController.m
+//  Apsiape
+//
+//  Created by Dario Lass on 06.05.13.
+//  Copyright (c) 2013 Bytolution. All rights reserved.
+//
+
+#import "BYMapViewController.h"
+
+@interface BYMapViewController ()
+
+@end
+
+@implementation BYMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
