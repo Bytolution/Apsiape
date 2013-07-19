0db9aadbbd0f7fc4501e804c16e2fffa791d5afa
Dario Lass <dev@dariolass.com>
2013-07-20 00:14:37 +0200

    BYStatsView WURKS!

--- Apsiape/BYStatsView.m
+++ Apsiape/BYStatsView.m
@@ -13,6 +13,8 @@
 @property (nonatomic, strong) NSArray *values;
 @property (nonatomic, strong) CALayer *beamContainer;
 
+- (void)executeBlockWithTimer:(NSTimer*)timer;
+
 @end
 
 @implementation BYStatsView
@@ -31,35 +33,45 @@
 {
     self.backgroundColor = [UIColor whiteColor];
     self.beamContainer = [CALayer layer];
-    CGRect frame = CGRectInset(self.bounds, 20, 20);
+    CGRect frame = CGRectInset(self.bounds, 20, 30);
     self.beamContainer.frame = frame;
     self.beamContainer.backgroundColor = [UIColor colorWithWhite:0.98 alpha:1].CGColor;
     float maxValue = [[self.values valueForKeyPath:@"@max.floatValue"] floatValue];
     for (int i = 0; i < self.values.count; i++) {
-        CALayer *beam = [CALayer layer];
+        __block CALayer *beam = [CALayer layer];
         CGFloat beamHeight = ([self.values[i] floatValue] * frame.size.height) / maxValue;
+        
+        CGRect baseFrame = CGRectMake((frame.size.width * ((float)i/(float)self.values.count)), 0, (frame.size.width/(float)self.values.count), 5);
+        baseFrame = CGRectInset(baseFrame, 8, 0);
+        
+        CGRect fromBounds = CGRectMake(0, 0, baseFrame.size.width, 5);
+        CGRect toBounds = CGRectMake(0, 0, baseFrame.size.width, beamHeight);
         
         beam.backgroundColor = [UIColor colorWithRed:0.5 green:0.6 blue:1 alpha:1].CGColor;
         beam.anchorPoint = CGPointMake(0, 0);
-        beam.position = CGPointMake(frame.size.width * ((float)i/(float)self.values.count), 0);
-        CGRect endBounds = CGRectMake(0, 0, frame.size.width /(float)self.values.count, beamHeight);
-        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"bounds"];
-        animation.fromValue = [NSValue valueWithCGRect:CGRectMake(0, 0, frame.size.width /(float)self.values.count, 5)];
-        animation.toValue = [NSValue valueWithCGRect:endBounds];
-        animation.duration = .5;
-        animation.beginTime = CACurrentMediaTime() + ((float)i * 2);
-        [beam addAnimation:animation forKey:@"beamAnimation"];
+        beam.position = baseFrame.origin;
+        beam.bounds = fromBounds;
+        
+        __block CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"bounds"];
+        animation.fromValue = [NSValue valueWithCGRect:fromBounds];
+        animation.toValue = [NSValue valueWithCGRect:toBounds];
+        animation.duration = 0.2;
+        
+        Float64 delayInSeconds = (float)i * 0.03;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void) {
+            beam.bounds = toBounds;
+            [beam addAnimation:animation forKey:@"bounds"];
+        });
         
         [self.beamContainer addSublayer:beam];
-        beam.bounds = endBounds;
     }
     [self.layer addSublayer:self.beamContainer];
 }
 
-- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
+- (void)executeBlockWithTimer:(NSTimer *)timer
 {
-    
+    NSLog(@"%s", __PRETTY_FUNCTION__);
 }
-
 
 @end
