918c517481b7cf8e6974e26967a47e9d7fd4e541
Dario Lass <dev@dariolass.com>
2013-07-23 01:44:48 +0200

    CFNetwork (old), BYLocalizer
    • removed old CFNetwork framework, readdition through Xcode 4.x.x
    • added BYLocalizer as starting point for dynamic localization features

---
+++ Apsiape/BYLocalizer.h
@@ -0,0 +1,13 @@
+//
+//  BYLocalizer.h
+//  Apsiape
+//
+//  Created by Dario Lass on 23.07.13.
+//  Copyright (c) 2013 Bytolution. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BYLocalizer : NSObject
+
+@end
---
+++ Apsiape/BYLocalizer.m
@@ -0,0 +1,13 @@
+//
+//  BYLocalizer.m
+//  Apsiape
+//
+//  Created by Dario Lass on 23.07.13.
+//  Copyright (c) 2013 Bytolution. All rights reserved.
+//
+
+#import "BYLocalizer.h"
+
+@implementation BYLocalizer
+
+@end
